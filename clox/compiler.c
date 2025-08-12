4ebae09fb3890ec6214234cd451d1842be925e5d
Eric <kitasuna@users.noreply.github.com>
2025-08-12 14:42:56 +0900

    Finish Ch19

--- clox/compiler.c
+++ clox/compiler.c
@@ -205,7 +205,7 @@ static void number() {
 
 static void string() {
 	emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
-																	parser.previous.length - 2));
+																	parser.previous.length - 2)));
 }
 
 static void unary() {
