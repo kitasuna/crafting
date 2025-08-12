92db1ed8968395948d2f786ce49376734634f69f
kitasuna <kitasuna@users.noreply.github.com>
2025-08-12 12:16:47 +0900

    finish ch20

--- clox/memory.h
+++ clox/memory.h
@@ -3,6 +3,11 @@
 
 #include "common.h"
 
+#define ALLOCATE(type, count) \
+  (type *)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 #define GROW_CAPACITY(capacity) \
   ((capacity) < 8 ? 8 : (capacity) * 2)
 
@@ -14,6 +19,7 @@
       sizeof(type) * (newCount))
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 
 
