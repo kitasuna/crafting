2642150fb218e1659d854aae6bddd9fdbf21b738
kitasuna <kitasuna@users.noreply.github.com>
2025-12-26 15:13:31 +0900

    Finish 26.3

--- clox/compiler.c
+++ clox/compiler.c
@@ -6,6 +6,7 @@
 #include "chunk.h"
 #include "common.h"
 #include "compiler.h"
+#include "memory.h"
 #include "object.h"
 #include "scanner.h"
 
@@ -795,5 +796,13 @@ ObjFunction* compile(const char* source) {
 	ObjFunction* function = endCompiler();
 
 	return parser.hadError ? NULL : function;
+}
+
+void markCompilerRoots() {
+	Compiler* compiler = current;
+	while (compiler != NULL) {
+		markObject((Obj*)compiler->function);
+		compiler = compiler->enclosing;
+	}
 }
 
