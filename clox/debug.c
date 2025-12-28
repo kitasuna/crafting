7c4d1e72739f0bf2837bddbeaa17fa27fbd9ec49
kitasuna <kitasuna@users.noreply.github.com>
2025-12-28 11:17:50 +0900

    Finish Ch27

--- clox/debug.c
+++ clox/debug.c
@@ -106,6 +106,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 			return constantInstruction("OP_GET_GLOBAL", chunk, offset);
 		case OP_SET_GLOBAL:
 			return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+		case OP_GET_PROPERTY:
+			return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+		case OP_SET_PROPERTY:
+			return constantInstruction("OP_SET_PROPERTY", chunk, offset);
 		case OP_GET_UPVALUE:
 			return byteInstruction("OP_GET_UPVALUE", chunk, offset);
 		case OP_SET_UPVALUE:
