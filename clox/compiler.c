0d5949586057ed54ea65d7cff1078304c305bc77
kitasuna <kitasuna@users.noreply.github.com>
2026-01-02 13:52:57 +0900

    Finish Ch28.3

--- clox/compiler.c
+++ clox/compiler.c
@@ -56,6 +56,7 @@ typedef struct {
 
 typedef enum {
 	TYPE_FUNCTION,
+	TYPE_METHOD,
 	TYPE_SCRIPT
 } FunctionType;
 
@@ -70,8 +71,13 @@ typedef struct Compiler {
 	int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+	struct ClassCompiler* enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler* current = NULL;
+ClassCompiler* currentClass = NULL;
 Chunk* compilingChunk;
 
 static Chunk* currentChunk() {
@@ -196,9 +202,14 @@ static void initCompiler(Compiler* compiler, FunctionType type) {
 
 	Local* local = &current->locals[current->localCount++];
 	local->depth = 0;
-	local->name.start = "";
-	local->name.length = 0;
 	local->isCaptured = false;
+	if (type != TYPE_FUNCTION) {
+		local->name.start = "this";
+		local->name.length = 4;
+	} else {
+		local->name.start = "";
+		local->name.length = 0;
+	}
 }
 
 static ObjFunction* endCompiler() {
@@ -517,7 +528,7 @@ static void namedVariable(Token name, bool canAssign) {
 static void method() {
 	consume(TOKEN_IDENTIFIER, "Expect method name.");
 	uint8_t constant = identifierConstant(&parser.previous);
-	FunctionType type = TYPE_FUNCTION;
+	FunctionType type = TYPE_METHOD;
 	function(type);
 	emitBytes(OP_METHOD, constant);
 }
@@ -531,6 +542,10 @@ static void classDeclaration() {
 	emitBytes(OP_CLASS, nameConstant);
 	defineVariable(nameConstant);
 
+	ClassCompiler classCompiler;
+	classCompiler.enclosing = currentClass;
+	currentClass = &classCompiler;
+
 	namedVariable(className, false);
 
 	consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
@@ -539,6 +554,7 @@ static void classDeclaration() {
 	}
 	consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
 	emitByte(OP_POP);
+	currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration() {
@@ -760,6 +776,15 @@ static void variable(bool canAssign) {
 	namedVariable(parser.previous, canAssign);
 }
 
+static void this_(bool canAssign) {
+	if (currentClass == NULL) {
+		error("Can't use 'this' outside of a class.");
+		return;
+	}
+
+	variable(false);
+}
+
 static void unary(bool canAssign) {
 	TokenType operatorType = parser.previous.type;
 
@@ -809,7 +834,7 @@ ParseRule rules[] = {
   [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
   [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
   [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
-  [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_THIS]          = {this_,    NULL,   PREC_NONE},
   [TOKEN_TRUE]          = {literal,  NULL,   PREC_NONE},
   [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
   [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
